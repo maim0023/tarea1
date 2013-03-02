cb976ad5f61b57b754e92c37c0208bc0e47260dd
maim0023 <maim0023@outlook.com>
2013-03-02 17:23:46 -0600

    Create Persona.cs
    clase persona

---
+++ Persona.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace tarea
+{
+  public class Persona
+	{
+		public string nombre;
+		public string  telefono;
+		public string email;
+		public string domicilio;
+		public Persona ()
+		{
+
+		}
+	}
+}
+
